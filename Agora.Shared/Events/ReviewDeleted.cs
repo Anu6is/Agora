5be846437f1a5cbdc7c50e71c705f4a95ce11bb8
Anu6is <curtis.mayers@gmail.com>
2022-10-13 06:16:45 -0400

    Add review deleted event handler

---
+++ Agora.Shared/Events/ReviewDeleted.cs
@@ -0,0 +1,36 @@
+﻿using Agora.Shared.Features.Commands;
+using Agora.Shared.Persistence.Models;
+using Emporia.Domain.Events;
+using Emporia.Extensions.Discord;
+using MediatR;
+
+namespace Agora.Shared.Events
+{
+    internal class ReviewDeleted : INotificationHandler<ReviewDeletedNotification>
+    {
+        private readonly IMediator _mediator;
+        private readonly IUserProfileService _userProfileService;
+
+        public ReviewDeleted(IUserProfileService userProfileService, IMediator mediator)
+        {
+            _mediator = mediator;
+            _userProfileService = userProfileService;
+        }
+
+        public async Task Handle(ReviewDeletedNotification notification, CancellationToken cancellationToken)
+        {
+            var profile = (UserProfile) await _userProfileService.GetUserProfileAsync(notification.EmporiumUser.EmporiumId.Value,
+                                                                                      notification.Review.ReferenceNumber.Value);
+
+            var score = notification.EmporiumUser.Reviews.GroupBy(x => x.Rating, (key, value) => key * value.Count()).Sum();
+            var reviews = notification.EmporiumUser.Reviews.Count;
+            var rating = Math.Round((decimal)score / reviews, 1);
+
+            profile.SetReviewCount((ulong)reviews).SetRating(rating);
+
+            await _mediator.Send(new UpdateUserProfileCommand(profile), cancellationToken);
+
+            return;
+        }
+    }
+}
