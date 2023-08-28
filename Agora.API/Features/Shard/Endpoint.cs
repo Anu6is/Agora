a2ffc105e9c48f98bdb814ed8174f28c3fcab048
Anu6is <curtis.mayers@gmail.com>
2023-08-28 18:50:28 -0400

    [Feature] Shard Status Endpoint - Retrieve the status of a shard via the API

---
+++ Agora.API/Features/Shard/Endpoint.cs
@@ -0,0 +1,24 @@
+﻿using Agora.Shared;
+using FastEndpoints;
+
+namespace Agora.API.Features.Shard
+{
+    public record ShardStatus(string State);
+
+    public class Endpoint : EndpointWithoutRequest<ShardStatus>
+    {
+        public IBotStatisticsService BotService { get; set; }
+
+        public override void Configure()
+        {
+            AllowAnonymous();
+            Get("/api/state/shard/{index}");
+        }
+
+        public override async Task HandleAsync(CancellationToken c)
+        {
+            int index = Route<int>("index");
+            await SendAsync(new ShardStatus(BotService.GetShardState(index)));
+        }
+    }
+}
